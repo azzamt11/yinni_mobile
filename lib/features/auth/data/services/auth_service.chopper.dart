8d16061216b1f476d45df922f34b4b95b9cf0e08
azzamt11 <dfeqhasavrymchaplctionnphysics@gmail.com>
2026-02-03 19:22:05 +0800

    fixing

--- lib/features/auth/data/services/auth_service.chopper.dart
+++ lib/features/auth/data/services/auth_service.chopper.dart
@@ -1,7 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // dart format width=80
 
-part of "auth_service.dart";
+part of 'auth_service.dart';
 
 // **************************************************************************
 // ChopperGenerator
