819ea98d1b78005f560fec26a7f05b6474b4fd94
azzamt11 <dfeqhasavrymchaplctionnphysics@gmail.com>
2026-02-05 19:59:29 +0800

    adapting kotlin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.yinni_mobile"
+    namespace = "io.yinni.yinni_mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.yinni_mobile"
+        applicationId = "io.yinni.yinni_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/io/yinni/yinni_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/io/yinni/yinni_mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.yinni_mobile
+package io.yinni.yinni_mobile
 
 import io.flutter.embedding.android.FlutterActivity
 
